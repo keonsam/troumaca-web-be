1739c947423b9857f9e50acd90211a4eb9815afa
keonsam <keonsamuel95@gmail.com>
2018-06-11 14:36:31 -0400

    appspec file updated

--- src/session/session.controller.ts
+++ src/session/session.controller.ts
@@ -7,7 +7,6 @@ export let getSimpleSession = (req: Request, res: Response) => {
   let sessionId = req.cookies["sessionId"];
   sessionOrchestrator.getSimpleSession(sessionId)
     .subscribe(next => {
-      res.setHeader('Content-Type', 'application/json');
       res.send(next);
     }, error => {
       res.status(400);
@@ -20,7 +19,6 @@ export let getPartyId = (req: Request, res: Response) => {
   let sessionId = req.cookies["sessionId"];
   sessionOrchestrator.getSimpleSession(sessionId)
     .subscribe(next => {
-      res.setHeader('Content-Type', 'application/json');
       res.send(JSON.stringify(next.partyId));
     }, error => {
       res.status(400);
@@ -33,7 +31,6 @@ export let isValidSession = (req: Request, res: Response) => {
   let sessionId = req.cookies["sessionId"];
   sessionOrchestrator.isValidSession(sessionId)
     .subscribe(next => {
-      res.setHeader('Content-Type', 'application/json');
       res.send(next);
     }, error => {
       res.status(400);
@@ -46,7 +43,6 @@ export let handleSessionLogOut = (req: Request, res: Response) => {
   let sessionId = req.cookies["sessionId"];
   sessionOrchestrator.handleSessionLogOut(sessionId)
     .subscribe(next => {
-      res.setHeader('Content-Type', 'application/json');
       res.send(next);
     }, error => {
       res.status(400);
