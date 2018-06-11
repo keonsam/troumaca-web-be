ea5b3c57d065c1b1bf0ffe09664f78c8d8975eab
keonsam <keonsamuel95@gmail.com>
2018-06-11 15:35:18 -0400

    appspec

--- bin/run.sh
+++ bin/run.sh
@@ -6,4 +6,4 @@ source /home/ec2-user/.bash_profile
 
 cd /home/ec2-user/app
 /usr/local/bin/npm install > /home/ec2-user/npm_install.log
-/usr/local/bin/forever start index.js
+/usr/local/bin/forever start server.js
