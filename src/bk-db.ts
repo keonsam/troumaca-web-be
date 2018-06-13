3e2978f0cdfa3d3109ccf1507ee171c833290ac9
keonsam <keonsamuel95@gmail.com>
2018-06-12 22:19:45 -0400

    build fix

--- src/bk-db.ts
+++ src/bk-db.ts
@@ -2,52 +2,52 @@ import path from 'path';
 import Datastore from 'nedb';
 
 // reference data
-let theDataTypesDb =  path.resolve(__dirname) + '/nedb/data-types.db';
-let theMeasuresDb = path.resolve(__dirname) + '/nedb/unit-of-measures.db';
+let theDataTypesDb =  path.resolve(__dirname,'..') + '/nedb/data-types.db';
+let theMeasuresDb = path.resolve(__dirname, '..') + '/nedb/unit-of-measures.db';
 
 // asset type
-let theAttributesDb = path.resolve(__dirname) + '/nedb/asset_type/attributes.db';
-let theAssignedAttributesDb = path.resolve(__dirname) + '/nedb/asset_type/assigned-attributes.db';
-let theAssetTypesDb = path.resolve(__dirname) + '/nedb/asset_type/asset-types.db';
-let theAssetTypeClassesDb = path.resolve(__dirname) + '/nedb/asset_type/asset-type-classes.db';
-let theValuesDb = path.resolve(__dirname) + '/nedb/asset_type/values.db';
-let theAssetsDb = path.resolve(__dirname) + '/nedb/asset_type/assets.db';
-let theAssetKindsDb = path.resolve(__dirname) + '/nedb/asset_type/asset-kinds.db';
+let theAttributesDb = path.resolve(__dirname, '..') + '/nedb/asset_type/attributes.db';
+let theAssignedAttributesDb = path.resolve(__dirname, '..') + '/nedb/asset_type/assigned-attributes.db';
+let theAssetTypesDb = path.resolve(__dirname,'..') + '/nedb/asset_type/asset-types.db';
+let theAssetTypeClassesDb = path.resolve(__dirname,'..') + '/nedb/asset_type/asset-type-classes.db';
+let theValuesDb = path.resolve(__dirname, '..') + '/nedb/asset_type/values.db';
+let theAssetsDb = path.resolve(__dirname, '..') + '/nedb/asset_type/assets.db';
+let theAssetKindsDb = path.resolve(__dirname, '..') + '/nedb/asset_type/asset-kinds.db';
 
 // sites
-let theSitesDb = path.resolve(__dirname) + '/nedb/site/sites.db';
-let theStreetAddressDb = path.resolve(__dirname) + '/nedb/site/street-addresses.db';
-let theTelephoneDb = path.resolve(__dirname) + '/nedb/site/telephones.db';
-let theEmailDb = path.resolve(__dirname) + '/nedb/site/emails.db';
-let theWebSiteDb = path.resolve(__dirname) + '/nedb/site/web-sites.db';
-let thePostOfficeBoxDb = path.resolve(__dirname) + '/nedb/site/post-office-boxes.db';
+let theSitesDb = path.resolve(__dirname, '..') + '/nedb/site/sites.db';
+let theStreetAddressDb = path.resolve(__dirname, '..') + '/nedb/site/street-addresses.db';
+let theTelephoneDb = path.resolve(__dirname, '..') + '/nedb/site/telephones.db';
+let theEmailDb = path.resolve(__dirname, '..') + '/nedb/site/emails.db';
+let theWebSiteDb = path.resolve(__dirname, '..') + '/nedb/site/web-sites.db';
+let thePostOfficeBoxDb = path.resolve(__dirname, '..') + '/nedb/site/post-office-boxes.db';
 
 // party
-let thePersonsDb = path.resolve(__dirname) + '/nedb/party/persons.db';
-let theOrganizationDb = path.resolve(__dirname) + '/nedb/party/organizations.db';
-let theUsersDb = path.resolve(__dirname) + '/nedb/party/users.db';
+let thePersonsDb = path.resolve(__dirname, '..') + '/nedb/party/persons.db';
+let theOrganizationDb = path.resolve(__dirname, '..') + '/nedb/party/organizations.db';
+let theUsersDb = path.resolve(__dirname, '..') + '/nedb/party/users.db';
 
 // authentication
-let theCredentialDb = path.resolve(__dirname) + '/nedb/authentication/credentials.db';
-let theCredentialConfirmationsDb = path.resolve(__dirname) + '/nedb/authentication/credential_confirmations.db';
+let theCredentialDb = path.resolve(__dirname, '..') + '/nedb/authentication/credentials.db';
+let theCredentialConfirmationsDb = path.resolve(__dirname, '..') + '/nedb/authentication/credential_confirmations.db';
 
 // file
-let theUserPhotosDb = path.resolve(__dirname) + '/nedb/file_meta_data/user-photos.db';
-let theOrganizationPhotosDb = path.resolve(__dirname) + '/nedb/file_meta_data/organization-photos.db';
+let theUserPhotosDb = path.resolve(__dirname, '..') + '/nedb/file_meta_data/user-photos.db';
+let theOrganizationPhotosDb = path.resolve(__dirname, '..') + '/nedb/file_meta_data/organization-photos.db';
 
-let sessionDb = path.resolve(__dirname,) + '/nedb/session/sessions.db';
+let sessionDb = path.resolve(__dirname, '..',) + '/nedb/session/sessions.db';
 
-let theShipmentsDb = path.resolve(__dirname,) + '/nedb/shipment/shipments.db';
+let theShipmentsDb = path.resolve(__dirname, '..',) + '/nedb/shipment/shipments.db';
 
 //authorization
-let accessRolesDb = path.resolve(__dirname,) + '/nedb/authorization/access-roles.db';
-let accessRoleTypesDb = path.resolve(__dirname,) + '/nedb/authorization/access-role-types.db';
-let grantsDb = path.resolve(__dirname,) + '/nedb/authorization/grants.db';
-let permissionsDb = path.resolve(__dirname,) + '/nedb/authorization/permissions.db';
-let resourcesDb = path.resolve(__dirname,) + '/nedb/authorization/resources.db';
-let resourceTypesDb = path.resolve(__dirname,) + '/nedb/authorization/resource-types.db';
-let resourcePermissionsDb = path.resolve(__dirname,) + '/nedb/authorization/resource-permissions.db';
-let partyAccessRolesDb = path.resolve(__dirname,) + '/nedb/authorization/party-access-roles.db';
+let accessRolesDb = path.resolve(__dirname, '..',) + '/nedb/authorization/access-roles.db';
+let accessRoleTypesDb = path.resolve(__dirname, '..',) + '/nedb/authorization/access-role-types.db';
+let grantsDb = path.resolve(__dirname, '..',) + '/nedb/authorization/grants.db';
+let permissionsDb = path.resolve(__dirname, '..',) + '/nedb/authorization/permissions.db';
+let resourcesDb = path.resolve(__dirname, '..',) + '/nedb/authorization/resources.db';
+let resourceTypesDb = path.resolve(__dirname, '..',) + '/nedb/authorization/resource-types.db';
+let resourcePermissionsDb = path.resolve(__dirname, '..',) + '/nedb/authorization/resource-permissions.db';
+let partyAccessRolesDb = path.resolve(__dirname, '..',) + '/nedb/authorization/party-access-roles.db';
 
 // Todo: Fix remove
 
