28bb2675e91c8d58e56a86bbbbdb6d28fbbc4675
keonsam <keonsamuel95@gmail.com>
2018-06-14 09:15:56 -0400

    appspec

--- src/app.ts
+++ src/app.ts
@@ -21,15 +21,15 @@ app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb',extended: false}));
 app.use(express.static(path.join(__dirname, "dist")));
 const whitelist = ["http://localhost:4200", "http://ec2-18-207-220-164.compute-1.amazonaws.com:4200"];
-
+function returnHost(origin:any) {
+    console.log(origin);
+    const index = whitelist.indexOf(origin);
+    if (index !== -1) {
+      return whitelist[index];
+    }
+}
 var corsOptions = {
-  origin: function (origin:any, callback:any) {
-      if (whitelist.indexOf(origin) !== -1) {
-          callback(null, true);
-      } else {
-          callback(new Error('Not allowed by CORS'));
-      }
-  },
+  origin: "http://localhost:4200,http://ec2-18-207-220-164.compute-1.amazonaws.com:4200",
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true
 };
