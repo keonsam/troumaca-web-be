75b55d16064b79b8b748006f76ad42820b54699f
keonsam <keonsamuel95@gmail.com>
2018-06-14 07:21:00 -0400

    cors fix

--- src/app.ts
+++ src/app.ts
@@ -20,8 +20,16 @@ app.use(cookieParser());
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb',extended: false}));
 app.use(express.static(path.join(__dirname, "dist")));
+const whitelist = ["http://localhost:4200", "http://ec2-18-207-220-164.compute-1.amazonaws.com:4200"];
+
 var corsOptions = {
-  origin: "http://localhost:4200",
+  origin: function (origin:any, callback:any) {
+      if (whitelist.indexOf(origin) !== -1) {
+          callback(null, true);
+      } else {
+          callback(new Error('Not allowed by CORS'));
+      }
+  },
   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   credentials: true
 };
